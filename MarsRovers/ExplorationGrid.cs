e45c8320f75fd341e8692077700c4bb5795c77ee
Greg Allen <gallen@prsmhosting.com>
2020-01-25 00:07:20 -0500

    Added code to establish an exploration grid.

---
+++ MarsRovers/ExplorationGrid.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRovers
+{
+    public class ExplorationGrid
+    {
+        public int EasternGridBound { get; set; }
+        public int NorthernGridBound { get; set; }
+    }
+}
--- MarsRovers/Program.cs
+++ MarsRovers/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MarsRovers
 {
     class Program
     {
+        static ExplorationGrid grid;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Messages.WELCOME_MESSAGE);
+            grid = new ExplorationGrid();
+            var gridEstablished = false;
+            while (!gridEstablished)
+            {
+                try
+                {
+                    gridEstablished = EstablishGrid();
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine(Messages.NORTHERN_BOUND_REQUIRED);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(Messages.POSITIVE_INTEGERS_ONLY);
+                }
+            }
+        }
+
+        static bool EstablishGrid()
+        {
+            Console.WriteLine(Messages.GRID_ESTABLISHMENT_INSTRUCTIONS);
+            var input = Console.ReadLine();
+            var coordinates = input.Split(" ");
+            grid.EasternGridBound = Convert.ToInt32(coordinates[0]);
+            grid.NorthernGridBound = Convert.ToInt32(coordinates[1]);
+            if (grid.EasternGridBound < 1 || grid.NorthernGridBound < 1)
+            {
+                throw new Exception();
+            }
+            return true;
         }
     }
 }
