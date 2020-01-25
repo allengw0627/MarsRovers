612d056588c261b2c89228788dc7f9abc940fa0e
Greg Allen <gallen@prsmhosting.com>
2020-01-25 12:40:25 -0500

    Converted string arrays to char arrays.

--- MarsRovers/Program.cs
+++ MarsRovers/Program.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace MarsRovers
 {
     class Program
     {
         static ExplorationGrid Grid;
+
+        static Rover Rover;
+
         static bool GridEstablished;
-        static List<string> Headings = new List<string>() { Constants.NORTH, Constants.EAST, Constants.SOUTH, Constants.WEST };
+
+        static bool CoordinatesAndHeadingConfirmed;
+
+        static bool RoverHasMoved;
+
+        static List<char> Headings = new List<char>() { Constants.NORTH, Constants.WEST, Constants.SOUTH, Constants.EAST };
+
         static void Main(string[] args)
         {
             Console.WriteLine(Constants.WELCOME_MESSAGE);
@@ -25,25 +35,107 @@ namespace MarsRovers
                 }
             }
 
-            while(1 < 2)
+            while (GridEstablished)
             {
-                try
+                while (!CoordinatesAndHeadingConfirmed)
                 {
-                    ConfirmCoordinatesAndHeading();
+                    try
+                    {
+                        ConfirmCoordinatesAndHeading();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
                 }
-                catch (Exception ex)
+                while (!RoverHasMoved)
                 {
-                    Console.WriteLine(ex.Message);
+                    try
+                    {
+                        MoveRover();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
                 }
             }
         }
 
-        static bool ValidInput(string[] input, int step)
+        static void EstablishGrid()
         {
-            var inputValid = false;
-            var inputAsInt32 = new int();
+            Console.WriteLine(Constants.GRID_ESTABLISHMENT_INSTRUCTIONS);
+            var coordinates = Console.ReadLine()
+                .ToCharArray();
+            ValidateInput(coordinates, Constants.GRID_ESTABLISHMENT_STEP);
+            Grid.EasternGridBound = Convert.ToInt32(coordinates[0]);
+            Grid.NorthernGridBound = Convert.ToInt32(coordinates[1]);
+            GridEstablished = true;
+        }
+
+        static void ConfirmCoordinatesAndHeading()
+        {
+            Console.WriteLine(Constants.ROVER_CONFIRMATION_INSTRUCTIONS);
+            var coordinatesAndHeading = Console.ReadLine()
+                .ToCharArray();
+            ValidateInput(coordinatesAndHeading, Constants.ROVER_COORDINATE_CONFIRMATION_STEP);
+            Rover = new Rover();
+            Rover.EastCoordinate = Convert.ToInt32(coordinatesAndHeading[0]);
+            Rover.NorthCoordinate = Convert.ToInt32(coordinatesAndHeading[1]);
+            Rover.Heading = coordinatesAndHeading[2];
+            CoordinatesAndHeadingConfirmed = true;
+        }
+
+        static void MoveRover()
+        {
+            Console.WriteLine(Constants.ROVER_MOVE_INSTRUCTIONS);
+            var turnMoveInstructions = Console.ReadLine()
+                .ToCharArray();
+            ValidateInput(turnMoveInstructions, Constants.ROVER_MOVE_STEP);
+            foreach (var instruction in turnMoveInstructions)
+            {
+                var currentHeadingIndex = Headings.IndexOf(Rover.Heading);
+                var newHeadingIndex = new int();
+                switch (instruction)
+                {
+                    case Constants.LEFT:
+                        newHeadingIndex = currentHeadingIndex++;
+                        newHeadingIndex = newHeadingIndex > 3 ? 0 : newHeadingIndex;
+                        Rover.Heading = Headings[newHeadingIndex];
+                        break;
+                    case Constants.RIGHT:
+                        newHeadingIndex = currentHeadingIndex--;
+                        newHeadingIndex = newHeadingIndex < 0 ? 3 : newHeadingIndex;
+                        Rover.Heading = Headings[newHeadingIndex];
+                        break;
+                    case Constants.MOVE:
+                        switch (Rover.Heading)
+                        {
+                            case Constants.NORTH:
+                                Rover.NorthCoordinate = Rover.NorthCoordinate++;
+                                break;
+                            case Constants.WEST:
+                                Rover.EastCoordinate = Rover.EastCoordinate--;
+                                break;
+                            case Constants.SOUTH:
+                                Rover.NorthCoordinate = Rover.NorthCoordinate--;
+                                break;
+                            case Constants.EAST:
+                                Rover.EastCoordinate = Rover.EastCoordinate++;
+                                break;
+                        }
+                        break;
+                }
+            }
+            RoverHasMoved = true;
+            CoordinatesAndHeadingConfirmed = false;
+            Console.WriteLine($"{Rover.EastCoordinate} {Rover.NorthCoordinate} {Rover.Heading}");
+        }
+
+        static void ValidateInput(char[] input, int step)
+        {
             var inputArray = input.ToList();
-            if (inputArray[0] == string.Empty && inputArray.Count() == 1)
+            if (char.IsWhiteSpace(inputArray[0]) && inputArray.Count() == 1)
             {
                 throw new Exception(Constants.INPUT_REQUIRED);
             }
@@ -54,20 +146,8 @@ namespace MarsRovers
                     {
                         throw new Exception(Constants.NORTHERN_BOUND_REQUIRED);
                     }
-                    foreach (var value in inputArray)
-                    {
-                        if (int.TryParse(value, out inputAsInt32))
-                        {
-                            if (inputAsInt32 < 1)
-                            {
-                                throw new Exception(Constants.POSITIVE_INTEGERS_ONLY);
-                            }
-                        }
-                        else
-                        {
-                            throw new Exception(Constants.POSITIVE_INTEGERS_ONLY);
-                        }
-                    }
+                    ValidateCoordinate(inputArray[0]);
+                    ValidateCoordinate(inputArray[1]);
                     break;
                 case Constants.ROVER_COORDINATE_CONFIRMATION_STEP:
                     if (inputArray.Count() < 2)
@@ -79,17 +159,8 @@ namespace MarsRovers
                         throw new Exception(Constants.HEADING_REQUIRED);
                     }
 
-                    int.TryParse(inputArray[0], out inputAsInt32);
-                    if (inputAsInt32 < 1)
-                    {
-                        throw new Exception(Constants.POSITIVE_INTEGERS_ONLY);
-                    }
-
-                    int.TryParse(inputArray[1], out inputAsInt32);
-                    if (inputAsInt32 < 1)
-                    {
-                        throw new Exception(Constants.POSITIVE_INTEGERS_ONLY);
-                    }
+                    ValidateCoordinate(inputArray[0]);
+                    ValidateCoordinate(inputArray[1]);
 
                     var heading = inputArray[2];
                     if (!Headings.Contains(heading))
@@ -98,36 +169,23 @@ namespace MarsRovers
                     }
 
                     break;
+                case Constants.ROVER_MOVE_STEP:
+                    foreach (var value in inputArray)
+                    {
+                        if (value != Constants.LEFT && value != Constants.RIGHT && value != Constants.MOVE)
+                        {
+                            throw new Exception(Constants.INSTRUCTION_INVALID);
+                        }
+                    }
+                    break;
             }
-            return inputValid;
         }
 
-        static void EstablishGrid()
+        static void ValidateCoordinate(char number)
         {
-            Console.WriteLine(Constants.GRID_ESTABLISHMENT_INSTRUCTIONS);
-            var coordinates = Console.ReadLine()
-                .Split(" ");
-            var validInput = ValidInput(coordinates, Constants.GRID_ESTABLISHMENT_STEP);
-            if (validInput)
+            if (!char.IsNumber(number) || number < 1)
             {
-                Grid.EasternGridBound = Convert.ToInt32(coordinates[0]);
-                Grid.NorthernGridBound = Convert.ToInt32(coordinates[1]);
-            }
-            GridEstablished = true;
-        }
-
-        static void ConfirmCoordinatesAndHeading()
-        {
-            Console.WriteLine(Constants.ROVER_CONFIRMATION_INSTRUCTIONS);
-            var coordinatesAndHeading = Console.ReadLine()
-                .Split(" ");
-            var validInput = ValidInput(coordinatesAndHeading, Constants.ROVER_COORDINATE_CONFIRMATION_STEP);
-            if (validInput)
-            {
-                var rover = new Rover();
-                rover.EastCoordinate = Convert.ToInt32(coordinatesAndHeading[0]);
-                rover.NorthCoordinate = Convert.ToInt32(coordinatesAndHeading[1]);
-                rover.Heading = coordinatesAndHeading[2];
+                throw new Exception(Constants.POSITIVE_INTEGERS_ONLY);
             }
         }
     }
